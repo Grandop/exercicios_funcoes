4a23fb3d3bbc9bc652eb190367ea21a1e526ffd6
Pedro Grando <grandop03@gmail.com>
2022-10-05 16:46:10 -0300

    Correção do  exercício 8

--- ex_funções.playground/Pages/Ex8.xcplaygroundpage/Contents.swift
+++ ex_funções.playground/Pages/Ex8.xcplaygroundpage/Contents.swift
@@ -1,17 +1,19 @@
 //: [Previous](@previous)
 func calculateAverage(_ tuples : (Double, Int)...) -> Double {
     var total: Double = 0
+    var weight: Int = 0
 
     for tuple in tuples {
         let grade = tuple.0 * Double(tuple.1)
         total += grade
+        weight += tuple.1
     }
     
     
-    return total / Double(tuples.count)
+    return total / Double(weight)
 }
     
 
-print(calculateAverage((10,1),(10, 1), (10, 1)))
+print(calculateAverage((8.2,3),(10, 2), (9.5, 4), (7.8,2), (10,2), (9.5,3), (6.7,4) ))
 
 //: [Next](@next)
