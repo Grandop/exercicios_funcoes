8804580c258c12432b71a6d6745d6d06e29ba31d
Pedro Grando <grandop03@gmail.com>
2022-10-05 23:57:06 -0300

    Exercício 9

---
+++ ex_funções.playground/Pages/Ex9.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+
+func convertDegrees(_ celsius: Int) -> Int {
+    var fahrenheit = 0
+    fahrenheit = (celsius * 9 / 5) + 32
+
+    return fahrenheit
+}
+
+print(convertDegrees(20))
+
+//: [Next](@next)
